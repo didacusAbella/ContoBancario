50f92464330c4b8b8c8487fa801e1d16decef2cc
Antonio <antonio.terrano8@gmail.com>
2017-02-12 19:04:59 +0100

    Riorganizzate classi, aggiunta configurazione eclipse.

---
+++ src/contobancario/bankaccounts/BankAccount.java
@@ -0,0 +1,156 @@
+package contobancario.bankaccounts;
+
+import contobancario.model.ClientRecord;
+
+/**
+ * The Class BankAccount.
+ *
+ */
+public class BankAccount {
+
+	/**
+	 * Initializes a new bank account.
+	 */
+	public BankAccount(){
+
+	}
+
+	/**
+	 * Initializes a new bank account with his client, balance and iban.
+	 * 
+	 * @param client the bank account's client
+	 * @param balance the bank account's balance
+	 * @param iban the bank account'e iban
+	 */
+	public BankAccount(ClientRecord client, double balance, String iban) {
+		this.accountHolder = client;
+		this.balance = balance;
+		this.iban = iban;
+	}
+
+	/**
+	 * Deposited the amount in the bank account.
+	 * 
+	 * @param ammount the amount that it must deposit
+	 */
+	public void deposit(double amount) {
+		if (amount < 0) 
+			throw new IllegalArgumentException("Can not deposit negative ammount!");
+		this.balance += amount;
+	}
+	
+	/**
+	 * Withdraw the amount from the bank account.
+	 * 
+	 * @param ammount the amount that it must withdraw
+	 */
+	public void withdraw(double amount) {
+		if (amount < 0 || amount > this.balance)
+			throw new IllegalArgumentException("Can not withdraw illegal ammount!");
+		this.balance -= amount;
+	}
+	
+	/**
+	 * Read the balance of the bank account.
+	 */
+	public void read() {
+		System.out.println("Your balance is: " + this.balance);
+	}
+
+	/**
+	 * @return the account holder
+	 */
+	public ClientRecord getAccountHolder() {
+		return accountHolder;
+	}
+
+	/**
+	 * @param accountholder the account holder to set
+	 */
+	public void setAccountholder(ClientRecord accountHolder) {
+		this.accountHolder = accountHolder;
+	}
+
+	/**
+	 * @return the balance
+	 */
+	public double getBalance() {
+		return balance;
+	}
+
+	/**
+	 * @param balance the balance to set
+	 */
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	/**
+	 * @return the iban
+	 */
+	public String getIban() {
+		return iban;
+	}
+
+	/**
+	 * @param iban the iban to set
+	 */
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+
+	/** 
+	 * Return a string representation of the structure object.
+	 * 
+	 * @return string representation of the object
+	 */
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + "[" + 
+				"accountHolder=" + this.accountHolder + 
+				", balance=" + this.balance + 
+				", iban=" + this.iban + 
+				"]";
+	}
+
+	/**
+	 * Make a "deep" comparison between this object and another object.
+	 * 
+	 * @return true, if the comparated object have the same class and the same properties
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BankAccount other = (BankAccount) obj;
+		return
+				this.accountHolder.equals(other.accountHolder) &&
+				this.balance == other.balance &&
+				this.iban.equals(other.balance);
+	}
+	
+	/**
+	 * Make a "deep" copy of this object.
+	 * 
+	 * @return cloned, the clone of this object
+	 */
+	@Override
+	public Object clone() {
+		try {
+			BankAccount cloned = (BankAccount) super.clone(); 
+			cloned.accountHolder = (ClientRecord) this.accountHolder.clone(); 
+			cloned.balance = this.balance;
+			cloned.iban = this.iban;
+			return cloned; 
+		}
+		catch (CloneNotSupportedException e) {
+			return null;
+		}
+	}
+
+	private ClientRecord accountHolder;
+	private double balance;
+	private String iban;
+}
--- src/contobancario/model/ClientRecord.java
+++ src/contobancario/model/ClientRecord.java
@@ -2,84 +2,143 @@ package contobancario.model;
 
 import java.util.GregorianCalendar;
 
+/**
+ * The Class ClientRecord.
+ */
 public class ClientRecord implements Cloneable {
+	
 	/**
-	 * Initialize a new client.
+	 * Initializes a new client.
 	 *
 	 */
-	public ClientRecord(){
+	public ClientRecord() {
 		
 	}
 	
 	/**
-	 * Inizialize a new client with his name, surname, birthday and residence.
+	 * Initializes a new client with his name, surname, birthday and residence.
 	 * 
-	 * @param name
-	 * @param surname
-	 * @param date
-	 * @param residence
+	 * @param name the client's name
+	 * @param surname the client's surname
+	 * @param date the client's birthday
+	 * @param residence the client's residence
 	 */
-	public ClientRecord(String name, String surname, GregorianCalendar date, String residence){
+	public ClientRecord(String name, String surname, GregorianCalendar date, String residence) {
 		this.name = name;
 		this.surname = surname;
 		this.birthday = date;
 		this.residence = residence;
 	}
-
+	
 	/**
-	 * 
-	 * @return the name.
+	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
-	 * Set the name.
-	 * 
-	 * @param name
+	 * @param name the name to set
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
 	/**
-	 * 
-	 * @return
+	 * @return the surname
 	 */
 	public String getSurname() {
 		return surname;
 	}
 
+	/**
+	 * @param surname the surname to set
+	 */
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
 
+	/**
+	 * @return the birthday
+	 */
 	public GregorianCalendar getBirthday() {
 		return birthday;
 	}
 
+	/**
+	 * @param birthday the birthday to set
+	 */
 	public void setBirthday(GregorianCalendar birthday) {
 		this.birthday = birthday;
 	}
 
+	/**
+	 * @return the residence
+	 */
 	public String getResidence() {
 		return residence;
 	}
 
+	/**
+	 * @param residence the residence to set
+	 */
 	public void setResidence(String residence) {
 		this.residence = residence;
 	}
 
+	/** 
+	 * Return a string representation of the structure object.
+	 * 
+	 * @return string representation of the object
+	 */
 	@Override
 	public String toString() {
-		return "ClientRecord [name=" + name + ", surname=" + surname + ", birthday=" + birthday + ", residence="
-				+ residence + "]";
+		return this.getClass().getSimpleName() + "[" +
+				"name=" + this.name + 
+				", surname=" + this.surname + 
+				", birthday=" + this.birthday + 
+				", residence=" + this.residence + 
+				"]";
 	}
 	
-	
+	/**
+	 * Make a "deep" comparison between this object and another object.
+	 * 
+	 * @return true, if the comparated object have the same class and the same properties
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClientRecord other = (ClientRecord) obj;
+		return 
+				this.name.equals(other.name) &&
+				this.surname.equals(other.surname) &&
+				this.birthday.equals(other.birthday) &&
+				this.residence.equals(other.residence);
+	}
 
-
+	/**
+	 * Make a "deep" copy of this object.
+	 * 
+	 * @return cloned, the clone of this object
+	 */
+	@Override
+	public Object clone() {
+		try {
+			ClientRecord cloned = (ClientRecord) super.clone(); 
+			cloned.name = this.name; 
+			cloned.surname = this.surname;
+			cloned.birthday = (GregorianCalendar) this.birthday.clone();
+			cloned.residence = this.residence;
+			return cloned; 
+		}
+		catch (CloneNotSupportedException e) {
+			return null;
+		}
+	}
 
 	private String name;
 	private String surname;
