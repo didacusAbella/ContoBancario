2143a4481ccb21b66de7a69cc1b50cf41028360f
Antonio <antonio.terrano8@gmail.com>
2017-02-24 01:14:27 +0100

    Update

--- src/contobancario/main/Main.java
+++ src/contobancario/main/Main.java
@@ -1,7 +1,12 @@
 package contobancario.main;
 
+import java.util.ArrayList;
+
+import contobancario.bankaccounts.BankAccount;
 import contobancario.exceptions.IllegalBankAccountException;
 import contobancario.gui.LoginFrame;
+import contobancario.gui.OperatorFrame;
+import contobancario.utils.CompareBankAccount;
 import contobancario.utils.Generator;
 
 public class Main {
@@ -9,6 +14,21 @@ public class Main {
 		
 		Generator generator = new Generator();
 		
-		LoginFrame login = new LoginFrame(generator);
+		//LoginFrame login = new LoginFrame(generator);
+		//OperatorFrame operator = new OperatorFrame(generator);
+		
+		ArrayList<BankAccount> bankaccouts = generator.getBankAccountks();
+		
+		for (BankAccount b: bankaccouts) {
+			System.out.println(b.getBalance());
+		}
+		
+		System.out.println("~~~");
+		
+		bankaccouts.sort(new CompareBankAccount());
+		
+		for (BankAccount b: bankaccouts) {
+			System.out.println(b.getBalance());
+		}
 	}
 }
